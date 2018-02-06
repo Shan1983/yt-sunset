d3a074c9cff46bf12efaacb5c7068acb34b3b19f
Shan1983 <s.galbraith08@gmail.com>
2018-02-07 02:44:47 +1100

    Continued with creating admin panel, added submenu and templates to helpkeep the project tidy

--- wp-content/themes/sunsettheme/inc/function-admin.php
+++ wp-content/themes/sunsettheme/inc/function-admin.php
@@ -7,19 +7,58 @@
  * =============
  */
 
- function sunset_add_admin_page() {
-    add_menu_page( 'Sunset Theme Options',
-    'Sunset',
-    'manage_options',
-    'alecaddd-sunset',
-    'sunset_theme_create_page',
-    get_template_directory_uri() . '/img/sunset-icon.png',
-    110);
- }
+function sunset_add_admin_page()
+{
+    /*
+     * creates the sunset admin page
+     */
 
- add_action('admin_menu', 'sunset_add_admin_page');
+    add_menu_page(
+        'Sunset Theme Options',
+        'Sunset',
+        'manage_options',
+        'alecaddd_sunset',
+        'sunset_theme_create_page',
+        get_template_directory_uri() . '/img/sunset-icon.png',
+        110
+    );
 
- function sunset_theme_create_page() {
-    echo '<h1>Sunset Theme Options</h1>';
- }
+    /*
+     * Create admin subpages
+     * Note: The first submenu needs to reflect the add_menu_page
+     * settings, otherwise you get funky formatting where the page
+     * is repeated. All further submenu's do not need to worry about this.
+     */
+
+    add_submenu_page(
+        'alecaddd_sunset',
+        'Sunset Theme Options',
+        'General',
+        'manage_options',
+        'alecaddd_sunset',
+        'sunset_theme_create_page'
+    );
+
+    add_submenu_page(
+        'alecaddd_sunset',
+        'Sunset CSS Options',
+        'Custom CSS',
+        'manage_options',
+        'alecaddd_sunset_css',
+        'sunset_theme_settings_page'
+    );
+
+}
+
+add_action('admin_menu', 'sunset_add_admin_page');
+
+function sunset_theme_create_page()
+{
+    require_once(get_template_directory() . '/inc/templates/sunset-admin.php');
+}
+
+function sunset_theme_settings_page()
+{
+    echo '<h1>Sunset CSS Settings</h1>';
+}
 
---
+++ wp-content/themes/sunsettheme/inc/templates/sunset-admin.php
@@ -0,0 +1,3 @@
+<h1><?php bloginfo(); ?> Theme Options</h1>
+<h3 class="title">Manage Options</h3>
+<p>Customize the default Wordpress Apperance Options</p>
